2978256499458487940126409c49bb37daa27fd8
Calvin Davis <mynameiscalvindavis@gmail.com>
2025-05-29 14:54:05 -0600

    Maybe i'll just make a million commits and my university professor can cry about git cleaniness

--- WaveFunctionCollapse/README.md
+++ WaveFunctionCollapse/README.md
@@ -1,6 +1,8 @@
 # Wave Function Collapse
 ### Simple Tiled Model / ESTM
-First pass at adjacency-rule WFC. It does the job, at present adjacency rules don't take into consideration rotation or reflection data because the domain reduction was minimal, based on how I'm already interpreting data in a cell. Ex: If a cell has data ABC, and another cell has data ABC, they both hash to the same identity despite being two separate objects... this and similar optimizations I think are what the original WFC algorithm solves with rotation and reflection data.
+First pass at adjacency-rule WFC. It does the job, at present adjacency rules don't take into consideration rotation or reflection data because the domain reduction was my main goal. Ex: If a cell has data ABC, and another cell has data ABC, they both hash to the same identity despite being two separate objects.
+
+My take at automatically generated adjacency rules was convoluted and not as interesting as the Overlapping WFC algorithm. I didn't generate a set of all tiles and THEN calculate adjacency (based on the edge colors of a tile), I just took the adjacent tiles within the image and made rules for neighbors. It was easier. It also meant that the domains of each cell were more restricted and it ran into superposition issues a lot more frequently.
 
 Derived from the following resources: <br>
 https://robertheaton.com/2018/12/17/wavefunction-collapse-algorithm/ <br>
--- WaveFunctionCollapse/Tile.py
+++ WaveFunctionCollapse/Tile.py
@@ -1,42 +1,119 @@
+import time
 
-HASH_TO_TILE = {}
-TILE_TO_HASH = {}
-HASH_TO_NAME = {} # 123123123 : "AA", printing purposes only
-NAME_TO_TILE = {}
 _NAMES = [x+y for x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for y in "1234567890"]
 
+OUTW = 15
+OUTH = 10
+CELL_SIZE = 40
+N = 3
+IMG = None
+IMG_WIDTH = None
+IMG_HEIGHT = None
+
+STATE_GRID = []
+ALL_TILES = []
+
+def setup():
+    global IMG
+    global IMG_WIDTH
+    global IMG_HEIGHT
+    size(OUTW*CELL_SIZE,OUTH*CELL_SIZE)
+    imgs = {
+        1:("Flower", 10,10),
+        2:("Pattern2", 3,3),
+        3:("Pattern3", 3,3),
+        4:("Pattern4", 3,3),
+        5:("Pattern5", 5,5),
+        6:("Pattern6", 4,4),
+        7:("FlowersBig", 46,46),
+        8:("BeachLand2", 20,10),
+        9:("BeachLand3", 10,5),
+        10:("BeachLand4", 4,7),
+        11:("BlueDots", 15,15),
+        12:("BlueDots2", 15,15),
+        13:("City", 9,9)
+    }
+    choice = 13
+
+    WIDTH = imgs[choice][1]
+    HEIGHT = imgs[choice][2]
+    IMG = load_image("patterns/{}.png".format(imgs[choice][0]))
+    IMG.load_pixels()
+    IMG_WIDTH = IMG.width
+    IMG_HEIGHT = IMG.height
+    extractAllTiles()
+
+def draw():
+    background(100)
+    for tile in ALL_TILES:
+        drawTile(tile=tile)
+    
+    for row in range(OUTH):
+        for col in range(OUTW):
+            x = col * CELL_SIZE
+            y = row * CELL_SIZE
+            stroke(100)
+            no_fill()
+            rect(x, y, CELL_SIZE, CELL_SIZE)
+
+def extractAllTiles():
+    for y in range(IMG_HEIGHT):
+        for x in range(IMG_WIDTH):
+            T = Tile(x,y, IMG)
+            T.extractColorData()
+            ALL_TILES.append(T)
+
+def drawTile(x=None,y=None,tile=None):
+    if not x or not y:
+        x = tile.img_x
+        y = tile.img_y
+    anchor_x = x * CELL_SIZE
+    anchor_y = y * CELL_SIZE
+    mini_cell_size = CELL_SIZE / N
+    for x in range(N):
+        for y in range(N):
+            col = tile.getColorFromData(x, y)
+            fill(col)
+            stroke(240)
+            rect(anchor_y + (y * mini_cell_size),
+                 anchor_x + (x * mini_cell_size), mini_cell_size, mini_cell_size)
+            
 class Tile:
     """
     A Tile represents a sliced-out chunk from an image. A Cell's domain contains multiple Tiles
     """
-    
-    def __init__(self, color_rows):
-        self.data = color_rows # NxN list of color values
+    def __init__(self, x,y, img, n=3):
+        """
+        x,y: x,y location on img
+        img: PImage object
+        """
+        self.data = []
+        self.name = None
+        self.img_x = x
+        self.img_y = y
         
-        HASH_TO_TILE[self.hash()] = self
-        TILE_TO_HASH[self] = self.hash()
-        for name in _NAMES:
-            if name not in list(HASH_TO_NAME.values()):
-                HASH_TO_NAME[self.hash()] = name
-                NAME_TO_TILE[name] = self
-                break
-        else:
-            raise # Run out of tile names, add more
+        self.pimg = img
+        self.n = n
         
-    def hash(self):
-        return hash(self.data)
+    def XYtoPImagePixelsIndex(self, x,y):
+        return (x % self.pimg.width) + (y % self.pimg.height) * self.pimg.width
+        
+    def extractColorData(self):
+        # Produce nxn section of self.img.pixels 
+        # (('#FFFFFFFF', '#FFFFFFFF', '#FFFFFFFF'),
+        #  ('#FFFFFFFF', '#000000FF', '#000000FF'),
+        #  ('#FFFFFFFF', '#000000FF', '#ED1C24FF'))
+        self.data = []
+        for i in range(self.n):
+            row = []
+            for j in range(self.n):
+                pix_index = self.XYtoPImagePixelsIndex(j + self.img_x,i + self.img_y)
+                row.append(hex_color(self.pimg.pixels[pix_index]))
+            self.data.append(tuple(row))
+        self.data = tuple(self.data)
     
-    def __str__(self):
-        return HASH_TO_NAME[self.hash()]
-    
-    def __repr__(self):
-        return self.__str__()
-    
-    def __hash__(self):
-        # I want collisions on identical data
-        return self.hash()
-    
-    def __del__(self):
-        # I anticipate this being called when I try to add a Tile to a dict,
-        # and it collides with an existing Tile, and disappears
-        print("This tile is being deleted from memory", self.hash())
+    def getColorFromData(self, x,y):
+        # Given self.data, return the x,y within
+        return self.data[y][x]
+
+
