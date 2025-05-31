624c3ab6fcb6c08f4718ba1a358c9f4a4bea67ba
Calvin Davis <mynameiscalvindavis@gmail.com>
2025-05-31 11:57:26 -0600

    Add Tile Adjacency

--- WaveFunctionCollapse/Tile.py
+++ WaveFunctionCollapse/Tile.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 _NAMES = [x+y for x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for y in "1234567890"]
 
@@ -6,6 +7,12 @@ OUTW = 15
 OUTH = 10
 CELL_SIZE = 40
 N = 3
+NORTH = (0,-1)
+EAST = (1,0)
+SOUTH = (0,1)
+WEST = (-1,0)
+
+
 IMG = None
 IMG_WIDTH = None
 IMG_HEIGHT = None
@@ -14,10 +21,12 @@ STATE_GRID = []
 ALL_TILES = []
 
 def setup():
+
     global IMG
     global IMG_WIDTH
     global IMG_HEIGHT
     size(OUTW*CELL_SIZE,OUTH*CELL_SIZE)
+    s_time = datetime.now()
     imgs = {
         1:("Flower", 10,10),
         2:("Pattern2", 3,3),
@@ -42,6 +51,11 @@ def setup():
     IMG_WIDTH = IMG.width
     IMG_HEIGHT = IMG.height
     extractAllTiles()
+    print(datetime.now() - s_time)
+    extractTileAdjacencies()
+    print(datetime.now() - s_time)
+    #print(ALL_TILES[0].isCompatible(ALL_TILES[9], "SOUTH"))
+    #print(ALL_TILES[30].dir_adj)
 
 def draw():
     background(100)
@@ -62,7 +76,16 @@ def extractAllTiles():
             T = Tile(x,y, IMG)
             T.extractColorData()
             ALL_TILES.append(T)
-
+    
+def extractTileAdjacencies():
+    # For every tile, for all 4 directions, identify what tiles I am adjacent to
+    for orig_tile in ALL_TILES:
+        # Not checking neighbors, checking ALL tiles in the dataset
+        for s in ["NORTH", "EAST", "SOUTH", "WEST"]:
+            for other_tile in ALL_TILES:
+                if orig_tile.isCompatible(other_tile, s):
+                    orig_tile.dir_adj[s].append(other_tile)
+            
 def drawTile(x=None,y=None,tile=None):
     if not x or not y:
         x = tile.img_x
@@ -87,10 +110,16 @@ class Tile:
         x,y: x,y location on img
         img: PImage object
         """
-        self.data = []
+        self.data = None
         self.name = None
         self.img_x = x
         self.img_y = y
+        self.dir_adj = {
+            "NORTH":[],
+            "SOUTH":[],
+            "EAST":[],
+            "WEST":[]
+        }
         
         self.pimg = img
         self.n = n
@@ -98,6 +127,43 @@ class Tile:
     def XYtoPImagePixelsIndex(self, x,y):
         return (x % self.pimg.width) + (y % self.pimg.height) * self.pimg.width
         
+    def isCompatible(self, other, d):
+        # DEFAULT j,i = [X X .] [X X .]
+        
+        # Sacrificing space for readability - the condensed version of this
+        # Would be less understandable
+        if d == "EAST": # Rows: all // Cols: [1:] == [:n-1]
+            for i in range(self.n):
+                for j in range(self.n-1):
+                    #print("Compare", j+1, i, "----", j,i)
+                    if self.getColorFromData(j+1, i) != other.getColorFromData(j, i):
+                        return False
+            return True
+        if d == "WEST": # Rows: all // Cols: [:n-1] == [1:]
+            for i in range(self.n):
+                for j in range(self.n-1):
+                    #print("Compare", j, i, "----", j,i)
+                    if self.getColorFromData(j, i) != other.getColorFromData(j+1, i):
+                        return False
+            return True
+        
+        if d == "NORTH": # Rows: [:n-1] == [1:] // Cols: all
+            for i in range(self.n-1):
+                for j in range(self.n):
+                    #print("Compare", j, i, "----", j,i+1)
+                    if self.getColorFromData(j, i) != other.getColorFromData(j, i+1):
+                        return False
+            return True
+        
+        if d == "SOUTH":
+            # This [1:] cols line up with other [:n-1] cols? [. X X] [X X .]
+            for i in range(self.n-1):
+                for j in range(self.n):
+                    #print("Compare", j, i+1, "----", j,i)
+                    if self.getColorFromData(j, i+1) != other.getColorFromData(j, i):
+                        return False
+            return True
+            
     def extractColorData(self):
         # Produce nxn section of self.img.pixels 
         # (('#FFFFFFFF', '#FFFFFFFF', '#FFFFFFFF'),
